0a64a782ad09b72039400a19afb38958f19a5f3f
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-05-17 19:43:34 +0200

    🚀 Backend hosts built frontend app

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,11 +1,34 @@
+"use-strict";
 import express, { Express, Request, Response } from "express";
-const app = express();
-const port = 80; // default port to listen
+import path from "path";
 
-// define a route handler for the default home page
-app.get("/", (req: Request, res: Response) => {
-	res.send("Express + TypeScript Server");
+const dirname = process.cwd();
+
+const app = express();
+const port = 80; // TODO : Move to config
+
+const protectedRoutes = ["/about"]; // TODO : Move to config
+
+app.use((req: Request, res: Response, next: Function) => {
+	const auth = true; // TODO : JWT
+
+	// TODO : Actually usefull logic
+	if (auth !== true && protectedRoutes.includes(req.path)) {
+		res.status(401);
+		res.send("Unauthorized");
+		return;
+	}
+
+	if (!req.path.includes(".") && req.path !== "/") {
+		console.log(req.path);
+		res.sendFile(path.join(dirname, "../Frontend/dist/index.html"));
+		return;
+	}
+
+	next();
 });
+
+app.use(express.static(path.join(dirname, "../Frontend/dist")));
 
 // start the Express server
 app.listen(port, () => {
