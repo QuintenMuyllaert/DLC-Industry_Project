66c0fae39c1a406e2b2f793be7ad0dc2900fda18
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-05-19 21:29:03 +0200

    🚨 Fixed Normal SVG => React SVG warnings

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -26,9 +26,9 @@ export const Login = () => {
 							height="24"
 							viewBox="0 0 24 24"
 							fill="none"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round">
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round">
 							<path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
 							<polyline points="10 17 15 12 10 7"></polyline>
 							<line x1="15" y1="12" x2="3" y2="12"></line>
@@ -45,9 +45,9 @@ export const Login = () => {
 							height="24"
 							viewBox="0 0 24 24"
 							fill="none"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round">
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round">
 							<path d="M2 12S5 5 12 5s10 7 10 7-3 7-10 7S2 12 2 12Z"></path>
 							<circle cx="12" cy="12" r="3"></circle>
 						</svg>
