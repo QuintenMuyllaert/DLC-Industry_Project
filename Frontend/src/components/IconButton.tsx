b93de8d20388385e7518d932c4464acd8127c7fb
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-05-20 15:54:58 +0200

    ✨ IconButton also supports icon only mode

--- Frontend/src/components/IconButton.tsx
+++ Frontend/src/components/IconButton.tsx
@@ -1,8 +1,12 @@
 import { ReactElement } from "React";
 
-export const IconButton = ({ label, color, icon, onClick }: { label: string; color: string; icon?: ReactElement; onClick?: (event?: any) => any }) => {
+export const IconButton = ({ label, color, icon, onClick }: { label?: string; color: string; icon?: ReactElement; onClick?: (event?: any) => any }) => {
+	if (!label) {
+		label = "";
+	}
+
 	return (
-		<button className={`c-iconbutton ${color} ${!icon ? "center" : ""}`} onClick={onClick ? onClick : () => {}}>
+		<button className={`c-iconbutton ${color} ${!icon ? "center" : ""} ${!label ? "nolabel" : ""}`} onClick={onClick ? onClick : () => {}}>
 			<>{icon ? icon : <></>}</>
 			{label}
 		</button>
