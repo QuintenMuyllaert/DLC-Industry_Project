8b06716ca562773732fd1768a1b8438dfdf7e9f4
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-05-24 13:41:51 +0200

    💄 Created search screen design

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import MatchSetup from "./pages/MatchSetup";
 import Score from "./pages/Score";
+import Search from "./pages/Search";
 
 import "./style/screen.scss";
 
@@ -13,6 +14,7 @@ export const App = () => {
 					<Route path="/" element={<Login />} />
 					<Route path="/score" element={<Score />} />
 					<Route path="/matchsetup" element={<MatchSetup />} />
+					<Route path="/search" element={<Search />} />
 				</Routes>
 			</div>
 		</Router>
