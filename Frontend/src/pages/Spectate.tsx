519c26015a5bf794ac83e35d8904e4bb31526720
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-05-24 15:53:21 +0200

    💄 Created spectate page + design

--- Frontend/src/pages/Spectate.tsx
+++ Frontend/src/pages/Spectate.tsx
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom";
-import { IconButton } from "../components/IconButton";
 import BottomTab from "../components/BottomTab";
 import Clock from "../components/Clock";
+import Flag from "../components/Flag";
 
 export const Spectate = () => {
 	return (
 		<>
-			<div className="p-spectate">
+			<div className="p-spectate maxwidth">
 				<header></header>
 				<Clock time="15:39" />
+				<div className="team">
+					<div className="flag">
+						<Flag top="#FF0000" bottom="#00FF5D" />
+					</div>
+					<h1 className="teamname">Torhout</h1>
+					<p>2</p>
+				</div>
+				<div className="team">
+					<h1 className="teamname">Kortrijk</h1>
+					<p>0</p>
+					<div className="flag">
+						<Flag top="#1900FF" bottom="#FFEE00" />
+					</div>
+				</div>
 			</div>
 			<BottomTab />
 		</>
