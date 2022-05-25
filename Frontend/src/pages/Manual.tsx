7b00c02e36d85b75eb695badd336dd72c28c5e76
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-05-25 14:49:41 +0200

    📱 Made manual scorebord input screen responsive

--- Frontend/src/pages/Manual.tsx
+++ Frontend/src/pages/Manual.tsx
@@ -32,32 +32,35 @@ export const Manual = () => {
 
 				<Logo width="4rem" height="4rem" />
 			</div>
-			<div className="u-grid-vertical-gap p-login-maxwidth">
-				<Input id="serienummer" label="serienummer" type="text" />
-				<Input id="username" label="e-mail" type="text" />
-				<Input id="password" label="wachtwoord" type="password" />
-				<Input id="confpassword" label="bevestig wachtwoord" type="password" />
-			</div>
-			<div className="u-grid-vertical-gap p-login-maxwidth">
-				<IconButton
-					icon={
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round">
-							<path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
-							<polyline points="10 17 15 12 10 7"></polyline>
-							<line x1="15" y1="12" x2="3" y2="12"></line>
-						</svg>
-					}
-					label="REGISTREREN"
-					color="white"
-				/>
+			<div className="content">
+				<div className="u-grid-vertical-gap">
+					<Input id="serienummer" label="serienummer" type="text" />
+					<Input id="username" label="e-mail" type="text" />
+					<Input id="password" label="wachtwoord" type="password" />
+					<Input id="confpassword" label="bevestig wachtwoord" type="password" />
+				</div>
+				<div className="button">
+					<IconButton
+						icon={
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round">
+								<path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+								<polyline points="10 17 15 12 10 7"></polyline>
+								<line x1="15" y1="12" x2="3" y2="12"></line>
+							</svg>
+						}
+						label="REGISTREREN"
+						color="white"
+					/>
+				</div>
 			</div>
 		</div>
 	);
