61b1dfb3ea455a9a46855de1e14ceb404a2d1155
Anton Labeeuw <anton.labeeuw@student.howest.be>
2022-05-30 14:07:12 +0200

    💄 Popup tekst aanpassen gemaakt HTML & CSS

--- Frontend/src/pages/Score.tsx
+++ Frontend/src/pages/Score.tsx
@@ -145,8 +145,8 @@ export const Score = () => {
 				</div>
 			</div>
 			<BottomTab />
-			<div className="c-colorpicker__overlay"></div>
-			<div className="c-colorpicker">
+			<div className="c-colorpicker__overlay c-colorpicker__hidden"></div>
+			<div className="c-colorpicker c-colorpicker__hidden">
 				<div className="c-colorpicker__container scrollbar">
 					<div className="c-colorpicker__close">
 						<svg
@@ -221,6 +221,50 @@ export const Score = () => {
 						</div>
 					</div>
 					<IconButton color="black" label="Opslaan"></IconButton>
+				</div>
+			</div>
+
+			<div className="c-textedit__overlay c-textedit__hidden"></div>
+			<div className="c-textedit c-textedit__hidden">
+				<div className="c-textedit__container">
+					<div className="c-textedit__header">
+						<p>Type hier je bericht:</p>
+						<div className="c-textedit__header-btn">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round">
+								<path d="M3 2v6h6"></path>
+								<path d="M3 13a9 9 0 1 0 3-7.7L3 8"></path>
+							</svg>
+
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round">
+								<line x1="18" y1="6" x2="6" y2="18"></line>
+								<line x1="6" y1="6" x2="18" y2="18"></line>
+							</svg>
+						</div>
+					</div>
+					<div className="c-textedit__textarea">
+						<label htmlFor="scrolltext">Tekst</label>
+						<textarea placeholder="Oude tekst..." name="scrolltext" id="scrolltext"></textarea>
+					</div>
+
+					<IconButton color="black" label="Bericht verzenden"></IconButton>
 				</div>
 			</div>
 		</>
