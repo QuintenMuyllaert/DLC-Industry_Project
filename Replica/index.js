d02c7c9805286df142e3fc478e7b5f5034a12a46
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-05-30 19:07:32 +0200

    🚨 Replica doesnt care about cors

--- Replica/index.js
+++ Replica/index.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(cors("*"));
 app.use(express.static(path.join(__dirname, "www")));
 
 let teams = [
