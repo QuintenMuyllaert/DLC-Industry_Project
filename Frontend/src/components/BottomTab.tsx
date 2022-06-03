b6778180b7a3c6c6b2a3da097127f3b777733f86
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-06-03 14:01:29 +0200

    🍻 Fixed navigation

--- Frontend/src/components/BottomTab.tsx
+++ Frontend/src/components/BottomTab.tsx
@@ -8,7 +8,11 @@ export const BottomTab = () => {
 	};
 
 	const goToUsers = async () => {
-		document.location.href = "/templates";
+		document.location.href = "/users";
+	};
+
+	const goToUserSettings = async () => {
+		document.location.href = "/usersettings";
 	};
 
 	return (
@@ -67,19 +71,19 @@ export const BottomTab = () => {
 				</svg>
 				<p className="c-bottomtab__page-name">Users</p>
 			</div>
-			<div className={document.location.pathname == "/usersettings" ? "c-bottomtab__page c-bottomtab__page-active" : "c-bottomtab__page"}>
+			<div
+				className={document.location.pathname == "/usersettings" ? "c-bottomtab__page c-bottomtab__page-active" : "c-bottomtab__page"}
+				onClick={goToUserSettings}>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
 					viewBox="0 0 24 24"
 					fill="none"
 					stroke="currentColor"
 					stroke-width="2"
 					stroke-linecap="round"
 					stroke-linejoin="round">
-					<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-					<circle cx="12" cy="7" r="4"></circle>
+					<path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path>
+					<circle cx="12" cy="12" r="3"></circle>
 				</svg>
 				<p className="c-bottomtab__page-name">Settings</p>
 			</div>
