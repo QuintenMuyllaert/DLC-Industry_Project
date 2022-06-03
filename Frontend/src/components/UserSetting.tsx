32961c540083b8774d1afa525ec91816173f2ca1
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-06-03 13:33:29 +0200

    💄 made the base layout and styling of the user settings page

---
+++ Frontend/src/components/UserSetting.tsx
@@ -0,0 +1,27 @@
+export const UserSetting = ({ content }: { content: string }) => {
+	return (
+		<article className="c-usersetting">
+			<div className="card">
+				<div className="content">
+					<p>{content}</p>
+				</div>
+				<svg
+					className="icon"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round">
+					<line x1="18" y1="2" x2="22" y2="6"></line>
+					<path d="M7.5 20.5 19 9l-4-4L3.5 16.5 2 22z"></path>
+				</svg>
+			</div>
+		</article>
+	);
+};
+
+export default UserSetting;
--- Frontend/src/pages/UserSettings.tsx
+++ Frontend/src/pages/UserSettings.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { LooseObject } from "../utils/Interfaces";
 import BottomTab from "../components/BottomTab";
+import UserSetting from "../components/UserSetting";
 
 export const UserSettings = () => {
 	const template: LooseObject = {};
@@ -12,7 +13,44 @@ export const UserSettings = () => {
 		setnewTemplate(newTemplate);
 	};
 
-	return <></>;
+	return (
+		<>
+			<div className="p-usersettings element">
+				<header className="userheader">
+					<div className="iconcontainer">
+						<svg
+							className="icon"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round">
+							<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+							<circle cx="12" cy="7" r="4"></circle>
+						</svg>
+					</div>
+					<h1>Hallo Jef</h1>
+				</header>
+				<div className="content">
+					<div className="item">
+						<p className="title">name:</p>
+						<UserSetting content="Jef" />
+					</div>
+					<div className="item">
+						<p className="title">email:</p>
+						<UserSetting content="Jef.Bezos@gmail.com" />
+					</div>
+					<div className="item">
+						<p className="title">wachtwoord:</p>
+						<UserSetting content="●●●●●●●●●●" />
+					</div>
+				</div>
+			</div>
+			<BottomTab />
+		</>
+	);
 };
 
 export default UserSettings;
