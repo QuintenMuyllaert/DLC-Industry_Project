d2268be08557cb27bebdccfc1dd0078c4af87758
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-06-04 20:00:25 +0200

    🐛 Made scrolltext slower

--- Frontend/src/components/Scrolltext.tsx
+++ Frontend/src/components/Scrolltext.tsx
@@ -1,7 +1,7 @@
 export const Scrolltext = ({ text }: { text: string }) => {
 	return (
 		<div className="c-scroll">
-			<div style={{ animationDuration: `${Math.max(0.25 * text.length, 2)}s` }} className="scrollcontainer">
+			<div style={{ animationDuration: `${Math.max(0.25 * text.length, 4)}s` }} className="scrollcontainer">
 				<h1>{text}</h1>
 			</div>
 			<h1 style={{ opacity: 0 }}>{text}</h1>
