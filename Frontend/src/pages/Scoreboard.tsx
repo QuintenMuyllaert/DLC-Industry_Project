78caddb2230bb19338dd9984cccd183b4e019984
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-06-09 15:32:52 +0200

    🚧 Fireworks WIP

--- Frontend/src/pages/Scoreboard.tsx
+++ Frontend/src/pages/Scoreboard.tsx
@@ -3,9 +3,58 @@ import Flag from "../components/Flag";
 import Scrolltext from "../components/Scrolltext";
 import { globalState } from "../utils/Appstate";
 
+import { CSSProperties } from "react";
+import { Fireworks, useFireworks } from "fireworks-js/dist/react";
+
 export const Scoreboard = () => {
+	const { enabled, options, setEnabled, setOptions } = useFireworks({
+		initialStart: true,
+		initialOptions: {
+			hue: {
+				min: 0,
+				max: 0,
+			},
+			delay: {
+				min: 20,
+				max: 50,
+			},
+			acceleration: 1.2,
+			friction: 0.96,
+			gravity: 1,
+			particles: 50,
+			trace: 3,
+			explosion: 6,
+			autoresize: true,
+			brightness: {
+				min: 1,
+				max: 50,
+				decay: {
+					min: 0.015,
+					max: 0.03,
+				},
+			},
+			boundaries: {
+				visible: false,
+			},
+		},
+	});
+
+	const style: CSSProperties = {
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		position: "fixed",
+		background: "#00000000",
+	};
+
+	if (enabled) {
+		setEnabled(false);
+	}
+
 	return (
 		<div className="p-scoreboard">
+			<Fireworks style={style} options={options} enabled={enabled} />
 			<div className="scoreboardtop">
 				<header>
 					<div className="team">
