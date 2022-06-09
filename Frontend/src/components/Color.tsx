baa1fec2c5aa3a3173c5d5024fd32257b6bd0caf
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-06-09 14:15:09 +0200

    💄 fixed icons on delete colpicker

--- Frontend/src/components/Color.tsx
+++ Frontend/src/components/Color.tsx
@@ -58,7 +58,22 @@ export const Color = ({
 
 							scoreboardInterface.changeColor(`${team}${side}`, Ecolor);
 					  }
-			}></div>
+			}>
+			<svg
+				// className="delete-icon"
+				className={state.isRemove ? "delete-icon" : ""}
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round">
+				<line x1="18" y1="6" x2="6" y2="18"></line>
+				<line x1="6" y1="6" x2="18" y2="18"></line>
+			</svg>
+		</div>
 	);
 };
 
