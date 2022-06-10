baa083609e0723f2a77dd118985ad92617622536
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-06-10 16:46:11 +0200

    ✏️ Fixed var typo in add user body

--- Frontend/src/components/User.tsx
+++ Frontend/src/components/User.tsx
@@ -6,7 +6,7 @@ export const User = ({ userName }: { userName: string }) => {
 			headers: {
 				"content-type": "application/json",
 			},
-			body: JSON.stringify({ userName, password: "password", serial: "X3462L7L" }),
+			body: JSON.stringify({ username: userName, password: "password", serial: "X3462L7L" }),
 		});
 	};
 
