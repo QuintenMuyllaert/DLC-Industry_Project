f15b41198e659bf093fa67d179418bfae14d2e30
Anton Labeeuw <anton.labeeuw@student.howest.be>
2022-06-10 13:29:02 +0200

    ✨ Add template werkt

--- Frontend/src/pages/Templates.tsx
+++ Frontend/src/pages/Templates.tsx
@@ -17,13 +17,17 @@ export const Templates = () => {
 
 	const handleClickNewTemplate = async () => {
 		// const res = await fetch(`/template?serial=X3462L7L`, { mode: "no-cors", method: "POST", body: JSON.stringify(template) });
-		console.log("dddddddddd", newTemplate);
-		const res = await fetch(`https://industryproject.computernetwork.be/template?serial=X3462L7L`, {
-			mode: "no-cors",
+		// console.log("dddddddddd", newTemplate);
+		const res = await fetch(`/template?serial=X3462L7L`, {
+			mode: "cors",
 			method: "POST",
+			cache: "no-cache",
+			credentials: "same-origin",
 			headers: {
 				"content-type": "application/json",
 			},
+			redirect: "follow",
+			referrerPolicy: "no-referrer",
 			body: JSON.stringify(newTemplate),
 		});
 	};
