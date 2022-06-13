f42e65855590c503af02cb4886aad21ac74ae5d8
Anton Labeeuw <anton.labeeuw@student.howest.be>
2022-06-13 16:40:55 +0200

    ⚗️ Navigate in commentaar om te testen

--- Frontend/src/pages/AddSponsorBundel.tsx
+++ Frontend/src/pages/AddSponsorBundel.tsx
@@ -58,6 +58,7 @@ export const AddSponsorBundel = () => {
 					<IconButton label="OPSLAAN" color="white" onClick={handleClickNewSponsorBundel} />
 				</div>
 			</div>
+
 			<BottomTab />
 		</>
 	);
