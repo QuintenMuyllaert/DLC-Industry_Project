703f5ec68f6965a08fe2c2fc1765ac5d0da28385
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-06-14 18:27:44 +0200

    💄 Fixed positioning of the close button on the clock popup

--- Frontend/src/components/ClockEdit.tsx
+++ Frontend/src/components/ClockEdit.tsx
@@ -48,7 +48,7 @@ export const ClockEdit = ({ active }: { active: boolean }) => {
 							viewBox="0 0 24 24"
 							fill="none"
 							stroke="currentColor"
-							strokeWidth="2"
+							strokeWidth="3"
 							strokeLinecap="round"
 							strokeLinejoin="round">
 							<line x1="18" y1="6" x2="6" y2="18"></line>
