5af9e1b42ded050a8af910d703038ad57fb6a265
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-06-14 10:30:39 +0200

    🎨 Added temporary locout icon to usersettings

--- Frontend/src/pages/UserSettings.tsx
+++ Frontend/src/pages/UserSettings.tsx
@@ -88,11 +88,27 @@ export const UserSettings = () => {
 
 	const onLogout = () => {
 		console.log("logging out");
+		document.location.href = "/logout";
 	};
 
 	return (
 		<>
 			<div className="p-usersettings element">
+				<div className="logout">
+					<svg
+						className="logout-icon"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+						<polyline points="16 17 21 12 16 7"></polyline>
+						<line x1="21" y1="12" x2="9" y2="12"></line>
+					</svg>
+				</div>
 				<header className="userheader">
 					<div className="iconcontainer">
 						<svg
